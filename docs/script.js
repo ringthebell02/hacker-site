4d3315c264f70ec82c1bc399f4f13234fa4f2837
gage bell <dougiebell0424@gmail.com>
2025-09-28 12:10:13 -0500

    Updated the icon boxes to show the full title

--- docs/script.js
+++ docs/script.js
@@ -6,29 +6,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const links = {
         games: [
-            { text: 'Internet Archive Flash', url: 'https://archive.org/details/softwarelibrary_flash_games' },
-            { text: 'C64 Games', url: 'https://archive.org/details/softwarelibrary_c64' },
-            { text: 'Console Emulator', url: 'https://archive.org/details/consolelivingroom' },
-            { text: 'Oregon Trail', url: 'https://www.visitoregon.com/the-oregon-trail-game-online/' },
-            { text: 'GitHub Unblocked Games List', url: 'https://gist.github.com/eddy-22/2cdfadfa539cdaccfd0a9e31bf98e6d6#unblocked-sites' },
-            { text: '', url: '#' },
-            { text: 'JavyQuavy Games', url: 'https://sites.google.com/view/javyquavy-un-blocked-games/home' },
+            { text: 'Internet Archive Flash Games', url: 'https://archive.org/details/softwarelibrary_flash_games' },
+            { text: 'Internet Archive Console Emulator', url: 'https://archive.org/details/consolelivingroom' },
             { text: 'RubyCave', url: 'http://Rubycave.com.ar' },
-            { text: 'Kelki (Drawing Tool)', url: 'https://kleki.com' },
-            { text: 'MORE COMING SOON', url: '#' },
-            
-           
+            { text: 'Oregon Trail', url: 'https://www.visitoregon.com/the-oregon-trail-game-online/' },
+            { text: 'Snap', url: 'https://snap.berkeley.edu/' },
+            { text: 'Unblocked Games Site 1', url: 'https://sites.google.com/view/javyquavy-un-blocked-games/home' },
         ],
         movies: [
-            { text: 'Movie/TV Site A', url: 'https://sites.google.com/view/empireunblockin/movies/page-10' },
-            { text: 'Movie/TV Site B', url: 'https://sites.google.com/view/scholarvision/movies' },
+            { text: 'Movie Site A', url: 'https://sites.google.com/view/empireunblockin/movies/' },
+            { text: 'Movie Site B', url: 'https://sites.google.com/view/scholarvision/movies' },
         ],
         suggestions: [
-            { text: 'Google Form', url: 'https://docs.google.com/forms/u/0/' }, // <-- IMPORTANT: Replace with your actual Google Form link
+            { text: 'Suggestions Form', url: 'https://docs.google.com/forms/d/e/1FAIpQLSdi2lo9oSEV6_Ngh_4cCRjwn4PFJ9XhBrKxk0T3fwFsRO7TFw/viewform?usp=header' },
         ],
         ai: [
             { text: 'MS Copilot', url: 'https://copilot.microsoft.com' },
-            { text: 'MORE COMING SOON', url: '#' },
         ],
         docs: [
             { text: 'Google Doc', url: 'https://docs.google.com/document/d/10AvkAcGyNWAI2-DoFeldyRJQoPX2W9eaf2dQlnvA4ds/edit?usp=sharing' },
